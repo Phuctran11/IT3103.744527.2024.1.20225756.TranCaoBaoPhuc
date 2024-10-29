82bb9fac1e568123ad76584e5022a897a2279f42
Phuctran11 <trancaobaophuc2019@gmai.com>
2024-10-30 00:00:22 +0700

    Upload lab2

---
+++ LAB2/AimsProject/Aims.java
@@ -0,0 +1,33 @@
+package aims;
+
+public class Aims {
+	public static void main(String[] args) {
+		Cart anOrder = new Cart();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("BaoPhuc-The Lion King",
+				"Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("BaoPhuc-Star Wars", 
+				"Science Fiction", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("BaoPhuc-Aladin",
+				"Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("BaoPhuc-Two broken girls",
+				"Comedy", "BaoPhuc", 22.22f);
+		anOrder.addDigitalVideoDisc(dvd4);
+		
+		System.out.println("Total cost is:");
+		System.out.println(anOrder.totalCost());
+		
+		anOrder.removeDigitalVideoDisc(dvd3);
+		
+		System.out.println("Total cost is:");
+		System.out.println(anOrder.totalCost());
+		
+		
+	}
+}
